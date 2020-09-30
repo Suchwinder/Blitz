c87aeeeb8eae8fce71aa0eb08a4b709c438a6a3a
Suchwinder Singh <windersingh80@yahoo.com>
2020-09-30 19:16:21 -0400

    set up skeleton server

--- back_end/main.py
+++ back_end/main.py
@@ -1,5 +1,5 @@
 from server import create_app
 
-if __name__ == "main":
+if __name__ == "__main__":
     app = create_app()
     app.run(debug = True)
--- back_end/server/__init__.py
+++ back_end/server/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
-    @app.route('./hello')
+    @app.route('/')
     def hello() :
         return "Hello World! Welcome to Blitz"
     
