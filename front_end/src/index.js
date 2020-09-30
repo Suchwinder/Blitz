9eb2d153788a07f9eccbea7e0b5b96fff6d6dce2
Suchwinder Singh <windersingh80@yahoo.com>
2020-09-30 17:11:09 -0400

    Initialized React Router for front end, and code refactor for flask

--- front_end/src/index.js
+++ front_end/src/index.js
@@ -4,10 +4,30 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+/**
+ * BrowserRouter is a version of react routing in order to match the
+ * UI(User Interface) with the URL selected.
+ * So it basically is one of many approaches 
+ * to ensure what we see on the webpage matches the link we are at.
+ * in other words
+ * This provides the routing ability to change the URL to the 
+ * appropriate link such that additional features can be loaded
+ * in for our SPA(Single Page Application). It allowes the user to move backwards and
+ * forwards in the webpage. Also provides deeplinking
+ * where hyperlinks lead to a specific content instead of a 
+ * general home page.
+ * 
+ * https://stackoverflow.com/questions/53065686/why-do-we-use-browserrouter-in-react
+ */
+import { BrowserRouter } from 'react-router-dom';
+
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>, 
   document.getElementById('root')
 );
 
