4989d0087107eef10d409fc2f0eea84d434a1c81
Kent Zhang <kntzhang@gmail.com>
2020-10-01 14:21:30 -0400

    implement navbar

--- front_end/src/components/main_page/MainPage.jsx
+++ front_end/src/components/main_page/MainPage.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import Form from 'react-bootstrap/Form'
 
 const MainPage = () => {
-    return (
-        <div>
-            <p> Blitz </p>
-        </div>
-    )
+  return(
+    <Navbar bg="primary" variant="dark">
+      <Navbar.Brand href="#home">Blitz</Navbar.Brand>
+      <Nav className="mr-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#create-group">Create Group</Nav.Link>
+        <Nav.Link href="#join-group">Join Group</Nav.Link>
+      </Nav>
+      <Form inline>
+      </Form>
+    </Navbar>
+)
+    
 }
 
 export default MainPage;
