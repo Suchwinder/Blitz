7e7696d79365a24040d2ced8e2f9f5dee4137dd7
Kent Zhang <kntzhang@gmail.com>
2020-10-02 10:11:05 -0400

    created pages folder
    Close Issue#12, 13, 14
    
    Co-Authored-By: Rohan Tohaan <32985555+rtohaan@users.noreply.github.com>

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import MainPage from './components/main_page/MainPage';
+import MainPage from './components/pages/main_page/MainPage';
+import CreateGroup from './components/pages/create_group/CreateGroup'
 import { Route, Switch } from 'react-router-dom';
 
 function App() {
@@ -12,7 +13,8 @@ function App() {
       <Switch> 
         <Route exact path="/" render={MainPageComponent}/>
         {
-        /*<Route path="/createGroup" component={CreateGroupComponent} />
+        /*
+        <Route path="/createGroup" component={CreateGroupComponent} />
         <Route path="/joinGroup" component={JoinGroupComponent} />
         <Route path="/splitBill" component={SplitBillComponent} /> */
         }
---
+++ front_end/src/components/pages/create_group/CreateGroup.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+
+const CreateGroup = () => {
+  return(
+    <header>
+      <Navbar bg="primary" variant="dark">
+        <Navbar.Brand href="#home">Blitz</Navbar.Brand>
+        <Nav className="ml-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#create-group">Create Group</Nav.Link>
+          <Nav.Link href="#join-group">Join Group</Nav.Link>
+        </Nav>
+      </Navbar>
+      
+    </header>
+    
+)   
+}
+
+export default CreateGroup;
