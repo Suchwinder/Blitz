cfd24345284192ed78e466c85396ece189ba3ace
Suchwinder Singh <windersingh80@yahoo.com>
2020-10-01 23:30:27 -0400

    Progress on database creation
    environment variable set up and accessible

--- back_end/models/__init__.py
+++ back_end/models/__init__.py
@@ -1,9 +1,17 @@
 import sqlalchemy 
 from sqlalchemy import create_engine # create instance of database
 from sqlalchemy.ext.declarative import declarative_base # create a catalog of all classes connected to tables
-from sqlalchemy import Column, Integer, String, DateTime, Float, Boolean, ForeignKey # Allows us to create columns, with datatypes, and aslo foreign keys
-from sqlalchemy.orm import relationship # Set up the relationships between our tables
+from sqlalchemy import Column, Integer, String, DateTime, Float, Boolean, ForeignKey # allows us to create columns, with datatypes, and aslo foreign keys
+from sqlalchemy.orm import relationship # set up the relationships between our tables
+from pathlib import Path # file path access
+from dotenv import load_dotenv # get the env variables loaded into system environment https://github.com/theskumar/python-dotenv#readme 
+import os # access the environment variables that were loaded
 
+# give path location
+env_path = Path('..')/'.env'
+load_dotenv(dotenv_path=env_path)
+
+DATABASE_URI = os.getenv('DATABASE_URI') # have saved variables 
 Base = declarative_base() # instantiating base catalog
 metadata = sqlalchemy.MetaData() # lets us use the information of the tables to create them later
 
