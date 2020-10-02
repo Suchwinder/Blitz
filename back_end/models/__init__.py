a603c017d1505816e7ee8966ef6743909f8085b9
davidy9000 <davidy.9000@gmail.com>
2020-10-01 23:09:39 -0400

    database: added relationships

--- back_end/models/__init__.py
+++ back_end/models/__init__.py
@@ -10,6 +10,8 @@ metadata = sqlalchemy.MetaData() # lets us use the information of the tables to 
 ### Database ORM Classes
 class Groups(Base):
     __tablename__ = 'groups'
+
+    # Columns
     groupID = Column(Integer, primary_key=True)
     groupURL = Column(String)
     locationID = Column(Integer, ForeignKey('locations.locationID'))
@@ -27,6 +29,8 @@ class Groups(Base):
 
 class Locations(Base):
     __tablename__ = 'locations'
+
+    # Columns
     locationID = Column(Integer, primary_key=True) 
     streetAddress = Column(String) 
     city = Column(String)
@@ -36,35 +40,62 @@ class Locations(Base):
     # Relationship(s) 
     group = relationship('Groups', uselist=False, backref='locations')
 
-class Zip(Base):
+class Zips(Base):
     __tablename__ = 'zips'
+
+    # Columns
     zipID = Column(Integer, primary_key=True)
     zipCode = Column(String)
     stateID = Column(Integer, ForeignKey('state.stateID'))
 
+    # Relationship(s)
+    locations = relationship('Locations', backref='zips')
+
 class State(Base):
     __tablename__ = 'state'
+
+    # Columns
     stateID = Column(Integer, primary_key=True)
     stateName = Column(String)
     taxRate = Column(Float)
 
+    # Relationships
+    zips = relationship('Zips', backref='state')
+
+
 class Users(Base):
     __tablename__ = 'users'
+
+    # Columns
     userID = Column(Integer, primary_key=True)
     nickname = Column(String)
     amountOwer = Column(Float)
     adjustedAmount = Column(Float)
     groupID = Column(Integer, ForeignKey('groups.groupID'))
 
+    # Relationships
+    groups = relationship('Groups', backref='users')
+
 class Items(Base):
     __tablename__ = 'items'
+
+    # Columns
     itemID = Column(Integer, primary_key=True)
     itemName = Column(String)
     itemCost = Column(Float)
     itemQuantity = Column(Integer)
     groupID = Column(Integer, ForeignKey('groups.groupID'))
 
+    # Relationships
+    groups = relationship('Groups', backref='items')
+
 class ItemAssignments(Base):
     __tablename__ = 'itemAssignments'
+
+    # Columns
     userID = Column(Integer, ForeignKey('users.userID'))
     itemID = Column(Integer, ForeignKey('items.itemsID'))
+
+    # Relationships
+    users = relationship('Users', backref='itemAssignments')
+    items = relationship('Items', backref='itemAssignments')
