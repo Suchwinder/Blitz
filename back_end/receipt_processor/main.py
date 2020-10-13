fc03dcc975077ffd361dda2603aedeebfe4a4957
davidy9000 <davidy.9000@gmail.com>
2020-10-13 05:42:35 -0400

    created basic functions for receipt processing

---
+++ back_end/receipt_processor/main.py
@@ -0,0 +1,30 @@
+from cv2 import cv2
+import numpy as np
+import pytesseract, re
+
+# Uses PytesseractOCR to gather text from images
+# Returns string
+def retrieveStringFromImage(receipt_image):
+    pass
+
+# Uses regular expression to get prices
+# Returns list of prices of type string
+def getCosts(receipt_text):
+    pass
+
+# Uses regular expression to get items
+# Returns list of items of type string
+def getItems(receipt_text):
+    pass
+
+# Create item & price pairs
+# Returns list of pairs
+def createItemCostPairs(items_list, price_list):
+    pass
+
+# Main function to run code
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
