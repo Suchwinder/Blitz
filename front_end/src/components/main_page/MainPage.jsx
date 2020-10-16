912ef5eb6704f51bdb3bb8658841abff33f41cb3
Kent Zhang <kntzhang@gmail.com>
2020-10-16 17:32:04 -0400

    changed hook to component

--- front_end/src/components/main_page/MainPage.jsx
+++ front_end/src/components/main_page/MainPage.jsx
@@ -1,25 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import NavBar from '../nav_bar/NavBar'
 import './MainPage.css'
 
-const MainPage = () => {
-  return(
-    <div className="main-page">
-      <NavBar/>
-      <div className="text">
-        <h1>What do we do?</h1>
-        <p>We help people split the bill.</p>
-        <h2>How it works:</h2>
-        <ol>
-          <li>Create a group</li>{" "}
-          <li>Enter Parameters (enter number of people)</li>{" "}
-          <li>Upload the receipt and share your group's link</li>{" "}
-          <li>Each individual selects their purchase(s)</li> <li>That's it</li>{" "}
-        </ol>{" "}
+class MainPage extends Component{
+  render(){
+    return(
+      <div className="main-page">
+        <NavBar/>
+        <div className="text">
+          <h1>What do we do?</h1>
+          <p>We help people split the bill.</p>
+          <h2>How it works:</h2>
+          <ol>
+            <li>Create a group</li>{" "}
+            <li>Enter Parameters (enter number of people)</li>{" "}
+            <li>Upload the receipt and share your group's link</li>{" "}
+            <li>Each individual selects their purchase(s)</li> <li>That's it</li>{" "}
+          </ol>{" "}
+        </div>
       </div>
-    </div>
-    
-)   
+    )   
+  }
+  
 }
 
 export default MainPage;
