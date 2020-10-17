974e1dcdb5a30226b6a5693d64248ac6f7562547
Kent Zhang <kntzhang@gmail.com>
2020-10-17 00:45:18 -0400

    changed from hook to component

--- front_end/src/components/create_group/CreateGroup.jsx
+++ front_end/src/components/create_group/CreateGroup.jsx
@@ -1,94 +1,107 @@
 import React from 'react';
 import NavBar from '../nav_bar/NavBar'
 import ChooseFile from '../upload_button/ChooseFile';
-import { useHistory } from "react-router-dom";
+// import { useHistory } from "react-router-dom";
 import './CreateGroup.css'
+import { Component } from 'react';
 
-// Todo: Have to move submit button
+// function HomeButton() {
+//   let history = useHistory();
 
-const CreateGroup = () => {
-  const history = useHistory();
+//   function handleClick() {
+//     history.push("/split_bill");
+//   }
+// }
 
-  const routeChange = () =>{ 
-    let path = `split_bill`; 
-    history.push(path);
+// for submit button: onClick={handleClick}
+// need to find a way to use the form to navigate to the bill splitting page
+
+class CreateGroup extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      names_for_split: "",
+      input_address: "",
+      state: "",
+    };
   }
-  return(
-    <div className="create-group-page">
-      <NavBar/>
-      <div className="text">
-  
-        <div>Creating Your Group</div>
-        <br></br>
-        <form>         
-          <label className="form" htmlFor="names">Input names for the split: {' '} </label>
-          <input type="text" id="names" name="names"></input>
+  render(){
+    return(
+      <div className="create-group-page">
+        <NavBar/>
+        <div className="text">
+          <div>Creating Your Group</div>
           <br></br>
-  <label className="form" htmlFor="address">Address(optional): {' '}</label>
-          <input type="text" id="address" name="address"></input>
-          <br></br>
-  <label className="form" htmlFor="states">State(optional): {' '}</label>
-          <select>
-            <option>Select</option>
-            <option value="AL">AL</option>
-            <option value="AK">AK</option>
-            <option value="AZ">AZ</option>
-            <option value="AR">AR</option>
-            <option value="CA">CA</option>
-            <option value="CO">CO</option>
-            <option value="CT">CT</option>
-            <option value="DE">DE</option>
-            <option value="FL">FL</option>
-            <option value="GA">GA</option>
-            <option value="HI">HI</option>
-            <option value="ID">ID</option>
-            <option value="IL">IL</option>
-            <option value="IN">IN</option>
-            <option value="IA">IA</option>
-            <option value="KS">KS</option>
-            <option value="KY">KY</option>
-            <option value="LA">LA</option>
-            <option value="ME">ME</option>
-            <option value="MD">MD</option>
-            <option value="MA">MA</option>
-            <option value="MI">MI</option>
-            <option value="MN">MN</option>
-            <option value="MS">MS</option>
-            <option value="MO">MO</option>
-            <option value="MT">MT</option>
-            <option value="NE">NE</option>
-            <option value="NV">NV</option>
-            <option value="NH">NH</option>
-            <option value="NJ">NJ</option>
-            <option value="NM">NM</option>
-            <option value="NY">NY</option>
-            <option value="NC">NC</option>
-            <option value="ND">ND</option>
-            <option value="OH">OH</option>
-            <option value="OK">OK</option>
-            <option value="OR">OR</option>
-            <option value="PA">PA</option>
-            <option value="RI">RI</option>
-            <option value="SC">SC</option>
-            <option value="SD">SD</option>
-            <option value="TN">TN</option>
-            <option value="TX">TX</option>
-            <option value="UT">UT</option>
-            <option value="VT">VT</option>
-            <option value="VA">VA</option>
-            <option value="WA">WA</option>
-            <option value="WV">WV</option>
-            <option value="WI">WI</option>
-            <option value="WY">WY</option>
-          </select>
-          <br></br>
-          <br></br>
-          <ChooseFile/>
-          <input className="submitButton" type="submit" value="Submit" onClick={routeChange}></input>
-        </form>
+          <form>         
+            <label className="form" htmlFor="names">Input names for the split: {' '} </label>
+            <input type="text" id="names" name="names"></input>
+            <br></br>
+            <label className="form" htmlFor="address">Address(optional): {' '}</label>
+            <input type="text" id="address" name="address"></input>
+            <br></br>
+            <label className="form" htmlFor="states">State(optional): {' '}</label>
+            <select>
+              <option>Select</option>
+              <option value="AL">AL</option>
+              <option value="AK">AK</option>
+              <option value="AZ">AZ</option>
+              <option value="AR">AR</option>
+              <option value="CA">CA</option>
+              <option value="CO">CO</option>
+              <option value="CT">CT</option>
+              <option value="DE">DE</option>
+              <option value="FL">FL</option>
+              <option value="GA">GA</option>
+              <option value="HI">HI</option>
+              <option value="ID">ID</option>
+              <option value="IL">IL</option>
+              <option value="IN">IN</option>
+              <option value="IA">IA</option>
+              <option value="KS">KS</option>
+              <option value="KY">KY</option>
+              <option value="LA">LA</option>
+              <option value="ME">ME</option>
+              <option value="MD">MD</option>
+              <option value="MA">MA</option>
+              <option value="MI">MI</option>
+              <option value="MN">MN</option>
+              <option value="MS">MS</option>
+              <option value="MO">MO</option>
+              <option value="MT">MT</option>
+              <option value="NE">NE</option>
+              <option value="NV">NV</option>
+              <option value="NH">NH</option>
+              <option value="NJ">NJ</option>
+              <option value="NM">NM</option>
+              <option value="NY">NY</option>
+              <option value="NC">NC</option>
+              <option value="ND">ND</option>
+              <option value="OH">OH</option>
+              <option value="OK">OK</option>
+              <option value="OR">OR</option>
+              <option value="PA">PA</option>
+              <option value="RI">RI</option>
+              <option value="SC">SC</option>
+              <option value="SD">SD</option>
+              <option value="TN">TN</option>
+              <option value="TX">TX</option>
+              <option value="UT">UT</option>
+              <option value="VT">VT</option>
+              <option value="VA">VA</option>
+              <option value="WA">WA</option>
+              <option value="WV">WV</option>
+              <option value="WI">WI</option>
+              <option value="WY">WY</option>
+            </select>
+            <br></br>
+            <br></br>
+            <ChooseFile/>
+            <input className="submitButton" type="submit" value="Submit"></input>
+          </form>
+        </div>
       </div>
-    </div>
-  )   
+    )   
+  }
 }
 
 export default CreateGroup;
