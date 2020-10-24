18afc9f007276f2f8d14504b5d070c55222976a4
Suchwinder Singh <windersingh80@yahoo.com>
2020-10-23 23:43:40 -0400

    Populated zip and state tables with data
    We only are focusing on one state and need all zipcodes for NY so created the means to do that

--- back_end/models/__init__.py
+++ back_end/models/__init__.py
@@ -1,5 +1,4 @@
-import sqlalchemy 
-import os # access the environment variables that were loaded
+import sqlalchemy, os, pandas # access the environment variables that were loaded
 from sqlalchemy import create_engine # create instance of database
 from sqlalchemy import Column, Integer, String, DateTime, Float, Boolean, ForeignKey # allows us to create columns, with datatypes, and aslo foreign keys
 from sqlalchemy.orm import relationship, sessionmaker # set up the relationships between our tables and set upa connection for each request
@@ -15,6 +14,13 @@ Base = declarative_base() # instantiating base catalog that houses all tables, s
 DATABASE_URI = os.getenv('DATABASE_URI') 
 engine = create_engine(DATABASE_URI, echo=False) # need to connect to server, explanation: when we want to connect to database, upon our application launching we need some object to contain the info about our database, this is waht this vairbale does. https://hackersandslackers.com/python-database-management-sqlalchemy/
 
+Session = sessionmaker(bind=engine) # this object can be used to make a session later on, using the engine as its configuration so each session will use the enginer to aquire its resources
+
+### Create a Database Connection after each request
+def create_db_connection():
+    session = Session() # generates a object that is used to make transactions to database
+    return session
+
 ### Database Creation
 def bootDB():    
     if not database_exists(engine.url):
@@ -22,17 +28,25 @@ def bootDB():
         print("Creating Database")
         Base.metadata.create_all(engine)
         print("Creating Tables")
+
+        # Set up States and Zips Tables
+        session = create_db_connection()
+
+        state_info = States(stateName = "NY", taxRate = 1.08875)
+        session.add(state_info) # need to add state first in order for the zips table to have a foreign key refernce to the state's table, otherwise throws error
+        
+        zip_data = pandas.read_csv('./static/ny_zips.csv')
+        for key, values in zip_data.iterrows():
+            location = Zips(zipCode = values.zip, stateID = 1) # only working with NY state
+            session.add(location) # registers transactions but doesnt yet communicate with database
+        
+        session.commit() # confirm everything is added, note flush() is called as a part of comit()
+        # https://stackoverflow.com/questions/4201455/sqlalchemy-whats-the-difference-between-flush-and-commit#:~:text=The%20session%20object%20registers%20transaction,to%20the%20database%20until%20session.&text=commit()%20commits%20(persists)%20those,to%20commit()%20(1). 
+
+        session.close() # end the connection and return it back to the pool of available connections
+
     else:
         print("Database Already Exists")
-
-Session = sessionmaker(bind=engine) # this object can be used to make a session later on, using the engine as its configuration so each session will use the enginer to aquire its resources
-
-### Create a Database Connection after each request
-def create_db_connection():
-    session = Session()
-    return session
-
-# Do not need to close session/connection manually, after commit it will close it
 
 ### Database ORM Classes
 class Groups(Base):
@@ -73,13 +87,13 @@ class Zips(Base):
     # Columns
     zipID = Column(Integer, primary_key=True)
     zipCode = Column(String)
-    stateID = Column(Integer, ForeignKey('state.stateID'))
+    stateID = Column(Integer, ForeignKey('states.stateID'))
 
     # Relationship(s)
     locations = relationship('Locations', backref='zips')
 
-class State(Base):
-    __tablename__ = 'state'
+class States(Base):
+    __tablename__ = 'states'
 
     # Columns
     stateID = Column(Integer, primary_key=True)
