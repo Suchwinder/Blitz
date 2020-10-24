b37eb9f0f2d963b155d5a969a0c3971f0717dfba
Suchwinder Singh <windersingh80@yahoo.com>
2020-10-23 23:44:48 -0400

    Some code refactor to and commenting

--- back_end/routes/create_group.py
+++ back_end/routes/create_group.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request # create a blueprint for the routes to be registered to, not necessary but ood for modularization of routes
-from back_end.models import create_db_connection # calling our helper function to create a connection to the databse to execute a request
-import random, string
+from back_end.models import create_db_connection, Groups # calling our helper function to create a connection to the databse to execute a request
+import random, string, os, boto3
 
 # used to group a bunch of relted views together
 # views in this case can count as code written to create various endpoints
@@ -12,12 +12,20 @@ def create_group():
     if db_connection:
         data = request.get_json()
 
+        # Need to create work with s3 to sore image
+        AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+        AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+        
         users = data.names
         street_address = data.streetAddress
-        state = data.state # not sure if needed
+        # state = data.state # not sure if needed
+        # hardcode taxrate 1.08875
+        state = "NY"
         city = data.city
         location_name = data.locationName
         zip = data.zip
+
+        # new_group = Groups()
         # some means to store image 
         # processing for items?
 
--- back_end/server/__init__.py
+++ back_end/server/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from models import bootDB
-from back_end.routes import create_group, join_group
+# from back_end.routes import create_group, join_group
 
 def create_app():
     bootDB() #start database (good to do database boot before app to ensure it exists before app works)
     app = Flask(__name__) #start app
 
-    app.register_blueprint(create_group.bp)
-    app.register_blueprint(join_group.bp)
+    # app.register_blueprint(create_group.bp)
+    # app.register_blueprint(join_group.bp)
 
     return app
