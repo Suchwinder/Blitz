2e0a24e9138860c10d68235bd24b2f76dcae9caa
Kent Zhang <kntzhang@gmail.com>
2020-10-28 17:21:32 -0400

    updated state management

--- front_end/src/components/grid/Grid.jsx
+++ front_end/src/components/grid/Grid.jsx
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import { FormControl, Select, InputLabel, MenuItem} from '@material-ui/core';
-import '../split_bill/SplitBill'
+import '../split_bill/SplitBill';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -30,127 +30,130 @@ const ColoredLine = ({ color }) => (
 );
 
 export default function AutoGrid() {
-  const classes = useStyles();
+    const classes = useStyles();
 
-  return (
-    <div className={classes.root}>
-      <Grid container spacing={3}>
-        <Grid item xs>
+    return (
+        <div className={classes.root}>
+        <Grid container spacing={3}>
+            <Grid item xs>
+                <Paper className={classes.paper}>
+                <div className="receipt">
+                    <img src="https://mdbootstrap.com/img/Photos/Others/placeholder.jpg" className="placeholder" alt="example placeholder"></img>
+                </div>
+                {/* Items
+                    <ul className="innerList">
+                        <li>
+                            5 Apples
+                        </li>
+                        <li>
+                            3 Oranges
+                        </li>
+                    </ul> */}
+                </Paper>
+            </Grid>
+            {/* <Grid item xs>
+                <Paper className={classes.paper}>
+                    $/unit
+                    <ul className="innerList">
+                        <li>
+                            $3
+                        </li>
+                        <li>
+                            $2
+                        </li>
+                    </ul>
+                </Paper>
+            </Grid> */}
+            <Grid item xs>
             <Paper className={classes.paper}>
-              Items
-                <ul className="innerList">
-                    <li>
-                        5 Apples
-                    </li>
-                    <li>
-                        3 Oranges
-                    </li>
-                </ul>
+                <FormControl>
+                    <InputLabel >User 1</InputLabel>
+                    <Select>
+                        <MenuItem>Item 1</MenuItem>
+                        <MenuItem>Item 2</MenuItem>
+                    </Select>
+                </FormControl>
+                <br></br>
+                <FormControl>
+                    <InputLabel >User 2</InputLabel>
+                    <Select>
+                        <MenuItem>Item 1</MenuItem>
+                        <MenuItem>Item 2</MenuItem>
+                    </Select>
+                </FormControl>
             </Paper>
+            </Grid>
         </Grid>
-        <Grid item xs>
+
+        <br></br>
+        <ColoredLine color="black" />
+        <br></br>
+
+        <Grid container spacing={3}>
+            <Grid item xs>
             <Paper className={classes.paper}>
-                $/unit
+                User 1
                 <ul className="innerList">
-                    <li>
-                        $3
-                    </li>
-                    <li>
-                        $2
-                    </li>
-                </ul>
-          </Paper>
+                        <li>
+                            1x Apple
+                        </li>
+                        <br></br>
+                        <li>
+                            Total: $3
+                        </li>
+                    </ul>
+                </Paper>
+            </Grid>
+            <Grid item xs>
+            <Paper className={classes.paper}>
+                User 2
+                <ul className="innerList">
+                        <li>
+                            2x Orange
+                        </li>
+                        <br></br>
+                        <li>
+                            Total: $4
+                        </li>
+                    </ul>
+                </Paper>
+            </Grid>
+            <Grid item xs>
+            <Paper className={classes.paper}>
+                User 3
+                <ul className="innerList">
+                        <li>
+                            4x Apple
+                        </li>
+                        <li>
+                            1x Orange
+                        </li>
+                        <br></br>
+                        <li>
+                            Total: $14
+                        </li>
+                    </ul>
+                </Paper>
+            </Grid>
+            <Grid item xs>
+            <Paper className={classes.paper}>
+                Grand Total
+                <ul className="innerList">
+                        <li>
+                            5x Apple
+                        </li>
+                        <li>
+                            3x Orange
+                        </li>
+                        <br></br>
+                        <li>
+                            Total: $21
+                        </li>
+                    </ul>
+                </Paper>
+            </Grid>
         </Grid>
-        <Grid item xs>
-          <Paper className={classes.paper}>
-            <FormControl>
-                <InputLabel >User 1</InputLabel>
-                <Select>
-                    <MenuItem>Item 1</MenuItem>
-                    <MenuItem>Item 2</MenuItem>
-                </Select>
-            </FormControl>
-            <br></br>
-            <FormControl>
-                <InputLabel >User 2</InputLabel>
-                <Select>
-                    <MenuItem>Item 1</MenuItem>
-                    <MenuItem>Item 2</MenuItem>
-                </Select>
-            </FormControl>
-          </Paper>
-        </Grid>
-      </Grid>
-
-      <br></br>
-      <ColoredLine color="black" />
-      <br></br>
-
-      <Grid container spacing={3}>
-        <Grid item xs>
-          <Paper className={classes.paper}>
-              User 1
-              <ul className="innerList">
-                    <li>
-                        1x Apple
-                    </li>
-                    <br></br>
-                    <li>
-                        Total: $3
-                    </li>
-                </ul>
-            </Paper>
-        </Grid>
-        <Grid item xs>
-          <Paper className={classes.paper}>
-              User 2
-              <ul className="innerList">
-                    <li>
-                        2x Orange
-                    </li>
-                    <br></br>
-                    <li>
-                        Total: $4
-                    </li>
-                </ul>
-            </Paper>
-        </Grid>
-        <Grid item xs>
-          <Paper className={classes.paper}>
-              User 3
-              <ul className="innerList">
-                    <li>
-                        4x Apple
-                    </li>
-                    <li>
-                        1x Orange
-                    </li>
-                    <br></br>
-                    <li>
-                        Total: $14
-                    </li>
-                </ul>
-            </Paper>
-        </Grid>
-        <Grid item xs>
-          <Paper className={classes.paper}>
-              Grand Total
-              <ul className="innerList">
-                    <li>
-                        5x Apple
-                    </li>
-                    <li>
-                        3x Orange
-                    </li>
-                    <br></br>
-                    <li>
-                        Total: $21
-                    </li>
-                </ul>
-            </Paper>
-        </Grid>
-      </Grid>
-      <br></br>
-    </div>
-  );
+        <br></br>
+        </div>
+    );
 }
--- front_end/src/components/join_group/JoinGroup.jsx
+++ front_end/src/components/join_group/JoinGroup.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import NavBar from "../nav_bar/NavBar";
-import { Button, Label } from "reactstrap";
-import FormControl from '@material-ui/core/FormControl';
+// import { Button, Label } from "reactstrap";
+// import FormControl from '@material-ui/core/FormControl';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import { Input } from "@material-ui/core";
 import "./JoinGroup.css";
@@ -10,46 +10,61 @@ import "./JoinGroup.css";
 
 class JoinGroup extends Component {
   constructor(props){
-    super(props)
+    super(props);
     this.state = {
       session_link: "",
-    }
-  }
-  joinSession = async () => {
-    const link = {
-      "session_link": this.state.session_link,
-    }
-    const response = await fetch('/api/join', {
-      method:"PUT",
-      body: JSON.stringify(link)
-    });
+    };
 
-    const status = response.status;
-
-    if(status === 400 || status === 500){
-      alert("Please fix your link");
-      return;
-    }
-    else{
-      this.setState({
-        
-      });
-    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+  handleChange(event) {
+    this.setState({session_link: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A link was submitted: ' + this.state.session_link);
+    event.preventDefault();
+  }
+
+  // joinSession = async () => {
+  //   const link = {
+  //     "session_link": this.state.session_link,
+  //   }
+  //   const response = await fetch('/api/join', {
+  //     method:"PUT",
+  //     body: JSON.stringify(link)
+  //   });
+
+  //   const status = response.status;
+
+  //   if(status === 400 || status === 500){
+  //     alert("Please fix your link");
+  //     return;
+  //   }
+  //   else{
+  //     this.setState({
+  //       session_link: link
+  //     });
+  //   }
+  // }
   render(){  
     return (
         <div className="join-group-page">
           <NavBar />
           <div className="text">
-            <FormControl>
-              <Label className="form" id="session_link" type="link">
+            <form onSubmit={this.handleSubmit}>
+              <label className="form">
                 Enter your invite link:
                 <br></br>
-                <Input type="text" name="link"></Input>
-              </Label>
-              <Button type="submit" onClick={this.joinSession}><a className="isDisabled" href="/split_bill">Submit form</a></Button>
+                <Input type="text" value={this.state.session_link} onChange={this.handleChange}></Input>
+              </label>
+              <br></br>
+              <input type="submit" value="Submit"/>
+              {/* <Button type="submit" onClick={this.joinSession}><a className="isDisabled" href="/split_bill">Submit form</a></Button> */}
               <FormHelperText id="my-helper-text">This will redirect you to your session.</FormHelperText>
-            </FormControl>
+            </form>
           </div>
         </div>
     );
