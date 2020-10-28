95916e0845f5e5ad2de1870edde710fed2de10c7
davidy9000 <davidy.9000@gmail.com>
2020-10-27 20:03:21 -0400

    implement core get_group functionatity

--- back_end/server/__init__.py
+++ back_end/server/__init__.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from models import bootDB
-from routes import upload_image, create_group #create_group, join_group, s3_test
+from routes import upload_image, create_group, get_group #create_group, join_group, s3_test
 
 def create_app():
     bootDB() #start database (good to do database boot before app to ensure it exists before app works)
     app = Flask(__name__) #start app
 
     app.register_blueprint(create_group.bp)
-    # app.register_blueprint(join_group.bp)
+    app.register_blueprint(get_group.bp)
     app.register_blueprint(upload_image.bp)
 
     return app
