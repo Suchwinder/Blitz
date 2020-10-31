f0257387872e1b22f0242582b3da81f114b6f6f2
Suchwinder Singh <windersingh80@yahoo.com>
2020-10-31 02:12:55 -0400

    Create User Finished, Front End adjusted for route name change, postman collections updated, and server has all blueprints registered to it at this moment

--- back_end/server/__init__.py
+++ back_end/server/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
 from models import bootDB
-from routes import upload_image, create_group #create_group, join_group, s3_test
+from routes import upload_image, create_group, users #create_group, join_group, s3_test
+
+# User Imports
+from routes.users import create_user
+
 
 def create_app():
     bootDB() #start database (good to do database boot before app to ensure it exists before app works)
@@ -9,5 +13,8 @@ def create_app():
     app.register_blueprint(create_group.bp)
     # app.register_blueprint(join_group.bp)
     app.register_blueprint(upload_image.bp)
+    
+    # Register User Functionality
+    app.register_blueprint(create_user.bp)
 
     return app
