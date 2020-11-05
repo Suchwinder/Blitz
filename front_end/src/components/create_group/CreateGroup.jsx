4d9066df1984990db1a012115f8b4b9e1ccbf4b2
Rohan Tohaan <32985555+rtohaan@users.noreply.github.com>
2020-11-05 11:37:56 -0500

    added tip n freesolo for users

--- front_end/src/components/create_group/CreateGroup.jsx
+++ front_end/src/components/create_group/CreateGroup.jsx
@@ -7,7 +7,7 @@ import Button from 'react-bootstrap/Button';
 import { Formik } from 'formik';
 import PlacesAutocomplete from '../places_autocomplete/PlacesAutocomplete';
 import { Redirect } from 'react-router-dom';
-
+import FreesoloUsers from '../freesolo_users/FreesoloUsers'
 
 
 
@@ -74,7 +74,7 @@ class CreateGroup extends Component {
       "street_address": this.state.input_address,
       "location_name": this.state.input_location,
       "image_s3url": this.state.preview,
-      "tip_rate": 10
+      "tip_rate": this.state.input_tip
     }
     console.log(data);
     // fetch("localhost:5000/api/create_group", {
@@ -121,12 +121,12 @@ class CreateGroup extends Component {
           <div>Creating Your Group</div>
           <br></br>
             <Form className="form" onSubmit={handleSubmit}>
-
+              
               <Form.Group controlId="form.input_users">
                 <Form.Label>
                   Input names here:
                 </Form.Label>
-              <Form.Control 
+              <FreesoloUsers  //Doesn't allow duplicates
                 type="text"
                 placeholder="Names"
                 name="input_users"
@@ -135,6 +135,42 @@ class CreateGroup extends Component {
                 isValid={touched.input_users && !errors.input_users}
                 as="textarea" rows="1" 
                 required = "Please enter at least one user"
+              /> 
+                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+              </Form.Group>
+
+              {errors.input_users && touched.input_users && errors.input_users}
+
+              <Form.Group controlId="form.input_address">
+                <Form.Label>Address:</Form.Label>
+                <PlacesAutocomplete
+                type="text"
+                placeholder="Address"
+                name="input_address"
+                onChange={handleChange}
+                value={values.input_address}
+                isValid={touched.input_address && !errors.input_address}
+                as="textarea" rows="1"
+                />
+                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+              </Form.Group>
+
+              {errors.input_address && touched.input_address && errors.input_address}
+
+              
+              <Form.Group controlId="form.input_location">
+                <Form.Label>
+                  Input location here:
+                </Form.Label>
+              <Form.Control 
+                type="text"
+                placeholder="Location Name"
+                name="input_location"
+                onChange={this.handleChange}
+                value={this.state.input_location}
+                isValid={touched.input_location && !errors.input_location}
+                as="textarea" rows="1" 
+                required = "Please enter your location"
               /> 
                 <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
               </Form.Group>
@@ -149,7 +185,7 @@ class CreateGroup extends Component {
                   onBlur={handleBlur}
                   value={this.state.select_state}
                   as="select">
-                  <option value="">Select a state</option>
+                  {/* <option value="">Select a state</option>
                   <option value="Alabama">Alabama</option>
                   <option value="Alaska">Alaska</option>
                   <option value="Arizona">Arizona</option>
@@ -180,9 +216,9 @@ class CreateGroup extends Component {
                   <option value="Nevada">Nevada</option>
                   <option value="New Hampshire">New Hampshire</option>
                   <option value="New Jersey">New Jersey</option>
-                  <option value="New Mexico">New Mexico</option>
+                  <option value="New Mexico">New Mexico</option> */}
                   <option value="New York">New York</option>
-                  <option value="North Carolina">North Carolina</option>
+                  {/* <option value="North Carolina">North Carolina</option>
                   <option value="North Dakota">North Dakota</option>
                   <option value="Ohio">Ohio</option>
                   <option value="Oklahoma">Oklahoma</option>
@@ -199,8 +235,24 @@ class CreateGroup extends Component {
                   <option value="Washington">Washington</option>
                   <option value="West Virginia">West Virginia</option>
                   <option value="Wisconsin">Wisconsin</option>
-                  <option value="Wyoming">Wyoming</option>
+                  <option value="Wyoming">Wyoming</option> */}
                 </Form.Control>
+                <Form.Group controlId="form.input_tip">
+                <Form.Label>Tip:</Form.Label>
+                <Form.Control 
+                type="text"
+                name="input_tip"
+                onChange={handleChange}
+                value={values.input_tip}
+                as="select" rows="1" 
+                >
+                <option value="">Select an amount</option>  
+                <option value="10">10%</option>
+                <option value="15">15%</option>
+                <option value="20">20%</option>
+                <option value="25">25%</option>
+                </Form.Control>
+              </Form.Group>
                 <br></br>
                 <div>
                   <input type="file" name="file" ref={this.inputFileRef} onChange={this.handleImage}/>
---
+++ front_end/src/components/freesolo_users/FreesoloUsers.jsx
@@ -0,0 +1,40 @@
+/* eslint-disable no-use-before-define */
+import React from 'react';
+import Chip from '@material-ui/core/Chip';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: 500,
+    '& > * + *': {
+      marginTop: theme.spacing(3),
+    },
+  },
+}));
+
+export default function Tags() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Autocomplete
+        multiple
+        id="input_users"
+        options={Names.map((option) => option.title)}
+        freeSolo
+        renderTags={(value, getTagProps) =>
+          value.map((option, index) => (
+            <Chip variant="outlined" label={option} {...getTagProps({ index })} />
+          ))
+        }
+        renderInput={(params) => (
+          <TextField {...params} variant="filled" label="Users" />
+        )}
+      />
+    </div>
+  );
+}
+
+const Names = [{title: "John Doe"}];
