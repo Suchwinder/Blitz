8276d67f560eb3d241fc38635c44f2ed5b165310
Suchwinder Singh <windersingh80@yahoo.com>
2020-11-06 21:14:55 -0500

    Join Group completed

--- front_end/src/components/join_group/JoinGroup.jsx
+++ front_end/src/components/join_group/JoinGroup.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import NavBar from "../nav_bar/NavBar";
-// import { Button, Label } from "reactstrap";
+import { Button } from "reactstrap";
 // import FormControl from '@material-ui/core/FormControl';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import { Input } from "@material-ui/core";
 import "./JoinGroup.css";
+import { Redirect } from "react-router-dom";
 
 // https://stackoverflow.com/questions/50644976/react-button-onclick-redirect-page
 
@@ -13,59 +14,45 @@ class JoinGroup extends Component {
     super(props);
     this.state = {
       session_link: "",
+      submit_link: "",
     };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event) {
-    this.setState({session_link: event.target.value});
+  handleChange = (event) =>{
+    this.setState({
+      session_link: event.target.value
+    });
   }
 
-  handleSubmit(event) {
-    alert('A link was submitted: ' + this.state.session_link);
-    event.preventDefault();
-  }
-
-  componentDidMount() {
-    fetch("/api/session_link")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            session_link: result.session_link
-          });
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
+  handleSubmit = () =>{
+    if(this.state.session_link.length === 0) {
+      alert("Please enter a link")
+    }
+    const start = this.state.session_link.search('/split_bill');
+    this.state.session_link.substring(start);
+    
+    this.setState({
+      submit_link: this.state.session_link,
+    }, () => console.log(this.state))
   }
 
   render(){  
+    if(this.state.submit_link !== "") {
+      return <Redirect to={this.state.submit_link}/>
+    }
     return (
         <div className="join-group-page">
           <NavBar />
           <div className="text">
-            <form onSubmit={this.handleSubmit}>
               <label className="form">
                 Enter your invite link:
                 <br></br>
                 <Input type="text" value={this.state.session_link} onChange={this.handleChange}></Input>
               </label>
               <br></br>
-              <input type="submit" value="Submit"/>
-              {/* <Button type="submit" onClick={this.joinSession}><a className="isDisabled" href="/split_bill">Submit form</a></Button> */}
+              {/* <input type="submit" value="Submit"/> */}
+              <Button type="submit" onClick={this.handleSubmit}>Join</Button>
               <FormHelperText id="my-helper-text">This will redirect you to your session.</FormHelperText>
-            </form>
           </div>
         </div>
     );
