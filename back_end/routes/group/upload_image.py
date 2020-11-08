a482a2276e21606e81b229dafa15484db69edd5e
Suchwinder Singh <windersingh80@yahoo.com>
2020-11-07 22:04:21 -0500

    Adding Credentials to Acess S3 bucket

--- back_end/routes/group/upload_image.py
+++ back_end/routes/group/upload_image.py
@@ -20,15 +20,15 @@ def upload():
         letters = str(uuid.uuid4())
 
         # Need to use Boto3 to use AWS services
-        # AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-        # AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+        AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+        AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
         BUCKET = os.getenv('BUCKET')
         # FOLDER = os.getenv('FOLDER')
         
         s3 = boto3.client(
-            's3'  # ,
-            # aws_access_key_id=AWS_ACCESS_KEY_ID,
-            # aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+            's3',
+            aws_access_key_id=AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=AWS_SECRET_ACCESS_KEY
         )  # specifying amazon resource
 
         object_url = None
