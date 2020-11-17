843ec1386806b9af3d732c68a0d4d6ba3792694e
Kent Zhang <kntzhang@gmail.com>
2020-11-17 14:42:43 -0500

    implementing wireframe design

--- front_end/src/components/main_page/MainPage.jsx
+++ front_end/src/components/main_page/MainPage.jsx
@@ -1,27 +1,86 @@
 import React, { Component } from 'react';
-import NavBar from '../nav_bar/NavBar'
-import './MainPage.css'
+import NavBar from '../nav_bar/NavBar';
+import Grid from '@material-ui/core/Grid';
+import { Typography } from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
+import people from './people.jpg';
+import { Button } from 'reactstrap';
+// import './MainPage.css';
+
+const styles = () => ({
+  root:{
+    
+  },
+  img:{
+    // zIndex: -1,
+    width: '25%',
+    height: '25%',
+    top: 0,
+    left: 0,
+  },
+  text:{
+    fontSize: '20px',
+    color: 'black',
+    position: 'static',
+    textAlign: 'center',
+  },
+  steps:{
+    justifyContent: 'center',
+  },
+});
 
 class MainPage extends Component{
   render(){
+    const { classes } = this.props;
     return(
-      <div>
+      <div className={classes.root}>
         <NavBar/>
-        <div className="text">
-          <h1>What do we do?</h1>
-          <p>We help people split the bill.</p>
-          <h2>How it works:</h2>
-          <ol>
-            <li>Create a group</li>{" "}
-            <li>Enter Parameters (enter number of people)</li>{" "}
-            <li>Upload the receipt and share your group's link</li>{" "}
-            <li>Each individual selects their purchase(s)</li> <li>That's it</li>{" "}
-          </ol>{" "}
+        <div className={classes.text}>
+          <Grid container>
+            <Grid item xs={12} sm={6}>
+              <br></br>
+              <Typography variant="h4">
+                Welcome to Blitz
+              </Typography>
+              <Typography>  
+                An easy-to-use platform to help distribute your group's costs. 
+              </Typography>
+              <Typography>
+                Click below to get started.
+              </Typography>
+              <br></br>
+              <Button>Get Started</Button>
+            </Grid>
+            <img className={classes.img} src={people} alt="people"/>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+              
+          </Grid>
+          <Typography variant="h4">
+              Get started with these simple steps:
+          </Typography>
+          <br></br>
+          <Grid container className={classes.steps}>
+            <Grid item xs={4} sm={2}>
+              <Typography>
+                1. Click 'Create' on the task bar above or the 'Get Started' button below to get started.
+              </Typography>
+            </Grid>
+            <Grid item xs={4} sm={2}>
+              <Typography>
+                2. Fill out all of the prompts. Submitting your receipt is optional.
+              </Typography>
+            </Grid>
+            <Grid item xs={4} sm={2}>
+              <Typography>
+                3. Submit and share! 
+              </Typography>
+            </Grid>
+          </Grid>
         </div>
       </div>
     )   
   }
-  
 }
 
-export default MainPage;
+export default withStyles(styles)(MainPage);
