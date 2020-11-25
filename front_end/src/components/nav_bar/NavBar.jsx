75916d0a0460620284138a89b0335f6804627475
Rohan Tohaan <32985555+rtohaan@users.noreply.github.com>
2020-11-24 20:20:48 -0500

    added spacing for navbar btn

--- front_end/src/components/nav_bar/NavBar.jsx
+++ front_end/src/components/nav_bar/NavBar.jsx
@@ -4,6 +4,8 @@ import Navbar from "react-bootstrap/Navbar";
 import { Redirect } from "react-router-dom";
 import { Button } from "reactstrap";
 import { Link } from 'react-scroll';
+import './NavBar.css';
+
 
 
 const NavBar = (props) => {
@@ -26,7 +28,7 @@ const NavBar = (props) => {
       <Navbar bg="dark" variant="dark">
         <Navbar.Brand onClick={() => setPage("main")}>Blitz</Navbar.Brand>
         <Nav className="ml-auto">
-          <Button variant="dark" onClick={() => setPage("main")}>
+          <Button variant="dark" className="btn_space" onClick={() => setPage("main")}>
             Home
           </Button>
           <Link activeClass= "active"
@@ -34,12 +36,15 @@ const NavBar = (props) => {
             spy={true}
             smooth={true}
             offset ={-50}
-            duration={500}><Button variant="dark">About</Button> </Link>
+            duration={500}><Button variant="dark" className="btn_space" onClick={() => setPage("about")}>
+              About
+              </Button> 
+              </Link>
 
-          <Button variant="dark" onClick={() => setPage("create")}>
+          <Button variant="dark" className="btn_space" onClick={() => setPage("create")}>
             Create
           </Button>
-          <Button variant="dark" onClick={() => setPage("join")}>
+          <Button variant="dark" className="btn_space" onClick={() => setPage("join")}>
             Join
           </Button>
         </Nav>
